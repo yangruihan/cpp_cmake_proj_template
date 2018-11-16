15bc42cd28ac91970d947f77d7d69121c66f303b
C.y <yangruihan@vip.qq.com>
2018-11-16 12:04:50 +0800

    init

---
+++ create_proj.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+echo "----------------------------
+--- Start Create Project ---
+----------------------------" \
+&& mkdir src \
+&& mkdir test \
+&& mkdir include \
+&& mkdir libs \
+&& touch README.md \
+&& mv CMakeLists_src.txt src/CMakeLists.txt \
+&& mv CMakeLists_test.txt test/CMakeLists.txt \
+&& cp main.cpp src/main.cpp \
+&& cp main.cpp test/main.cpp \
+&& rm main.cpp \
+&& rm -rf .git \
+&& git init \
+&& echo "-----------------------------
+--- Finish Create Project ---
+-----------------------------" \
+&& echo "--- Please replace CMakeLists.txt proj_name with your project name"
